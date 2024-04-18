8e1e3f972c9333dc494fbd0ac0c64d6de812ccac
Dylan Rafferty <draff1800@gmail.com>
2024-04-17 23:37:00 -0400

    early work on controller endpoints

--- src/main/java/com/example/springbootmongodbdockerawsdemo/controller/GroceryItemController.java
+++ src/main/java/com/example/springbootmongodbdockerawsdemo/controller/GroceryItemController.java
@@ -2,6 +2,7 @@ package com.example.springbootmongodbdockerawsdemo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springbootmongodbdockerawsdemo.model.GroceryItem;
@@ -15,9 +16,18 @@ public class GroceryItemController {
     @Autowired
     GroceryItemService groceryItemServiceImpl;
 
-    @GetMapping("/")
-    public List<GroceryItem> testEndpoint() {
+    @GetMapping
+    public List<GroceryItem> getAllGroceryItems() {
         return groceryItemServiceImpl.getAllGroceryItems();
     }
 
+    @GetMapping("/category/{category}")
+    public List<GroceryItem> getGroceryItemsByCategory(@PathVariable String category) {
+        return groceryItemServiceImpl.getGroceryItemsByCategory(category);
+    }
+
+    @GetMapping("/name/{name}")
+    public GroceryItem getGroceryItemByName(@PathVariable String name) {
+        return groceryItemServiceImpl.getGroceryItemByName(name);
+    }
 }
